3a378dcc7b4c14f7b1140de81d04cace9ee3fb5a
fpatwa <farhan.patwa@utsa.edu>
2013-11-14 15:23:02 -0600

    Working updates

--- keystone/aws/aws_federated_token.py
+++ keystone/aws/aws_federated_token.py
@@ -9,8 +9,8 @@ print(iamconn)
 roles = iamconn.list_roles()
 #
 print("The available roles are:")
-print(roles.list_roles_response.list_roles_result.roles[0].arn)
-role1 = roles.list_roles_response.list_roles_result.roles[0].arn
+print(roles.list_roles_response.list_roles_result.roles[1].arn)
+role1 = roles.list_roles_response.list_roles_result.roles[1].arn
 #
 #
 
@@ -18,6 +18,15 @@ stsconnection = boto.sts.connect_to_region('us-east-1')
 #fd_token  = stsconnection.get_federation_token("khalid")
 fd_token = stsconnection.assume_role(role1, 'session1')
 
-print(fd_token.federated_user_arn)
-print(fd_token.packed_policy_size)
+print("\nThe Token user details are:")
+print(fd_token.user)
+print("User ARN: ", fd_token.user.arn)
+print("Assume Role ID: ", fd_token.user.assume_role_id)
+
+print("\nThe Token Credential details are:")
+print(fd_token.credentials)
+print("Access Key: ", fd_token.credentials.access_key)
+print("Secret Key: ", fd_token.credentials.secret_key)
+print("Session Token: ", fd_token.credentials.session_token)
+print("Expiration: ", fd_token.credentials.expiration)
 
